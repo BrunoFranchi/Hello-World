de05821a132827b5f8fa98ec5656154c8b834b96
BrunoFranchi <bruno-rogerio1@hotmail.com>
2020-01-07 16:04:45 -0300

    Add files via upload

---
+++ Curriculo.py
@@ -0,0 +1,18 @@
+import selenium
+import pandas
+import json
+
+
+dados_iniciais = {
+	"informacoes_basicas":[
+    "Email: Bruno Rogerio Franchi Rocha",
+    "Endereco: Rua Solemar 168 - Ipiranga",
+    "Data de Nascimento: 22/12/1989"
+	]
+
+}
+
+def habilidades(self):
+
+
+
